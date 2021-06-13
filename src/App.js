1be9f4457148cc316870270f454db899468f5cd4
ThaoNP <npthao1312@gmail.com>
2021-06-13 21:09:17 +0700

    :star: display linked pages properly

--- src/App.js
+++ src/App.js
@@ -134,8 +134,8 @@ class App extends Component {
                 }}
                 onClose={this.handleClose}
               >
-                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                <MenuItem component={Link} to="/profile" onClick={this.handleClose}>Profile</MenuItem>
+                <MenuItem component={Link} to="/login" onClick={this.logOut}>Logout</MenuItem>
               </Menu>
             </div>
             )}
@@ -173,7 +173,7 @@ class App extends Component {
               <ListItemIcon><PeopleIcon /></ListItemIcon>
               <ListItemText primary="Groups" />
             </ListItem>
-            <ListItem button key="Files" component={Link} to="/files">
+            <ListItem button key="Files" component={Link} to="/add-files">
               <ListItemIcon><FileCopyIcon /></ListItemIcon>
               <ListItemText primary="Files" />
             </ListItem>
@@ -195,41 +195,17 @@ class App extends Component {
 
         <main className={classes.content}>
           <div className={classes.toolbar} />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-            vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-            hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-            tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-            nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-          </Typography>
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/users" component={UsersList} />
+            <Route exact path="/add" component={AddUser} />
+            <Route path="/users/:id" component={User} />
+            <Route exact path="/add-files" component={UploadFiles} />
+          </Switch>
         </main>
-
-        <Switch>
-          <Route exact path={["/", "/home"]} component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/users" component={UsersList} />
-          <Route exact path="/add" component={AddUser} />
-          <Route path="/users/:id" component={User} />
-          <Route exact path="/add-files" component={UploadFiles} />
-        </Switch>
       </div>
     );
   }
