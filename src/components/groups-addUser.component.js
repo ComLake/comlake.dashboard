c64b5e6032cfc410016016f2ac71617e27d75a51
ThaoNP <npthao1312@gmail.com>
2021-06-20 18:14:26 +0700

    :star: add autocomplete to group

--- src/components/groups-addUser.component.js
+++ src/components/groups-addUser.component.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import GroupDataService from "../services/group.service";
+import UserDataService from "../services/user.service";
 
 import { styles } from "../css-common"
 import { Card, Typography, TextField, CardHeader, CardContent,
   Button, CardActions, Paper, Chip, withStyles } from "@material-ui/core";
+  import Autocomplete from '@material-ui/lab/Autocomplete';
 import AddIcon from '@material-ui/icons/Add';
 import FaceIcon from '@material-ui/icons/Face';
 
@@ -19,18 +21,19 @@ class GroupAddUser extends Component {
                 id: null,
                 users: [],
             },
-            groupId: null,
+            users:[],
             username: null,
         };
     }
 
     componentDidMount() {
         this.getGroup(this.props.match.params.id);
+        this.retrieveUsers();
     }
 
-    onChangeUsername(e) {
+    onChangeUsername(event, value) {
       this.setState({
-        username: e.target.value
+        username: value.username
       });
     }
 
@@ -38,8 +41,7 @@ class GroupAddUser extends Component {
         GroupDataService.get(id)
             .then(response => {
                 this.setState({
-                    currentGroup: response.data,
-                    groupId: response.data.id
+                    currentGroup: response.data
                 });
             })
             .catch(e => {
@@ -47,9 +49,21 @@ class GroupAddUser extends Component {
             });
     }
 
+    retrieveUsers() {
+      UserDataService.getAll()
+        .then(response => {
+          this.setState({
+            users: response.data
+          });
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+
     addMember() {
         GroupDataService.addUser(
-            this.state.groupId,
+            this.state.currentGroup.id,
             this.state.username
         )
             .then(response => {
@@ -61,9 +75,8 @@ class GroupAddUser extends Component {
     }
 
     render() {
-        const { groupId, username, currentGroup } = this.state;
+        const { users, username, currentGroup } = this.state;
         const { classes } = this.props
-        console.log(groupId);
         return (
           <div>
             {currentGroup && (
@@ -75,7 +88,6 @@ class GroupAddUser extends Component {
                     <div>
                         <TextField
                             className={classes.textField}
-                            label="Name"
                             name="name"
                             variant="outlined"
                             margin="normal"
@@ -95,13 +107,14 @@ class GroupAddUser extends Component {
                       ))}
                     </Paper>
                     <div>
-                        <TextField
-                            className={classes.textField}
-                            label="Username"
-                            name="username"
-                            variant="outlined"
-                            margin="normal"
-                            onChange={this.onChangeUsername}
+                        <Autocomplete
+                          options={users}
+                          getOptionLabel={(users) => users.username}
+                          style={{ width: 300 }}
+                          onChange={this.onChangeUsername}
+                          renderInput={(params) =>
+                            <TextField {...params} margin="normal" label="Username" variant="outlined"/>
+                          }
                         />
                     </div>
                 </CardContent>
